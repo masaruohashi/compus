cb6d21b69b49615c709f80d9b1d67503c2897007
Rodrigo Masaru Ohashi <ohashi.masaru@gmail.com>
2017-11-17 15:39:51 -0200

    change the connection factory to use the mariadb database

--- src/br/com/compus/jdbc/ConnectionFactory.java
+++ src/br/com/compus/jdbc/ConnectionFactory.java
@@ -8,8 +8,8 @@ public class ConnectionFactory {
   public static Connection getConnection() {
     Connection connection = null;
     try {
-      Class.forName("com.mysql.jdbc.Driver");
-      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/compus", "root", "");
+      Class.forName("org.mariadb.jdbc.Driver");
+      connection = DriverManager.getConnection("jdbc:mariadb://143.107.102.5:3306/t2g5", "t2g5", "ttYMT5++");
     } catch (SQLException | ClassNotFoundException e) {
       e.printStackTrace();
     }
