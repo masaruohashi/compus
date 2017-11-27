8c8caaf765fe5608367ef29f97be727600576284
Rodrigo Masaru Ohashi <ohashi.masaru@gmail.com>
2017-11-27 12:28:26 -0200

    modify the existing tests to use Mockito and Powermock

--- test/br/com/compus/dao/EmployeeDAOTest.java
+++ test/br/com/compus/dao/EmployeeDAOTest.java
@@ -1,24 +1,41 @@
 package br.com.compus.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import br.com.compus.jdbc.ConnectionFactory;
 import br.com.compus.models.Employee;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ConnectionFactory.class)
 public class EmployeeDAOTest {
-  private Employee mockedEmployee = new Employee();
+  private EmployeeDAO dao;
+  private Connection connection;
+  private PreparedStatement statement;
+  private ResultSet result;
   
   @Before
   public void setUp() {
-    mockedEmployee.setName("Jonh Doe");
-    mockedEmployee.setCpf("111.111.111-11");
-    mockedEmployee.setEmail("john@doe.com");
-    mockedEmployee.setRole("Administrador");
+    connection = Mockito.mock(Connection.class);
+    statement = Mockito.mock(PreparedStatement.class);
+    result = Mockito.mock(ResultSet.class);
+    PowerMockito.mockStatic(ConnectionFactory.class);
+    Mockito.when(ConnectionFactory.getConnection()).thenReturn(connection);
+    dao = Mockito.spy(new EmployeeDAO());
   }
 
   @Test
@@ -27,34 +44,18 @@ public class EmployeeDAOTest {
   }
   
   @Test
-  public void testCreate() {
-    
-    try {
-    assertTrue(EmployeeDAO.getInstance().create(mockedEmployee));
-    } catch (SQLException e) {
-    e.printStackTrace();
-    }
+  public void testGetAll() throws SQLException {
+    Mockito.when(connection.prepareStatement("SELECT * FROM employee"))
+    .thenReturn(statement);
+    Mockito.when(statement.executeQuery()).thenReturn(result);
+    Mockito.when(result.next()).thenReturn(true).thenReturn(false);
+    Mockito.when(result.getInt("id")).thenReturn(1);
+    Mockito.when(result.getString("name")).thenReturn("name");
+    Mockito.when(result.getString("cpf")).thenReturn("111.111.111-1");
+    Mockito.when(result.getString("email")).thenReturn("email@email");
+    Mockito.when(result.getString("role")).thenReturn("role");
+    List<Employee> employees = dao.getAll();
+    assertEquals(1, employees.size());
+    assertEquals("name", employees.get(0).getName());
   }
-  
-  @Test
-  public void testGetAll() {
-    List<Employee> employees= null;
-    try {
-      employees = EmployeeDAO.getInstance().getAll();
-      assertTrue(employees.size() > 0);
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Test
-  public void testDelete() {
-    try {
-    assertTrue(EmployeeDAO.getInstance().delete(1));
-    } catch (SQLException e) {
-    e.printStackTrace();
-    }
-  }
- 
-
 }
