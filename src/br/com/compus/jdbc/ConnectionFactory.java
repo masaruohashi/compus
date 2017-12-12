786a338e1ce6f52f1ac6d37bae1bcf2d108744e7
BrunoSaito <bruno.saito@usp.br>
2017-12-11 23:05:48 -0200

    fixed ConnectionFactory credentials

--- src/br/com/compus/jdbc/ConnectionFactory.java
+++ src/br/com/compus/jdbc/ConnectionFactory.java
@@ -9,8 +9,7 @@ public class ConnectionFactory {
     Connection connection = null;
     try {
       Class.forName("org.mariadb.jdbc.Driver");
-//      connection = DriverManager.getConnection("jdbc:mariadb://143.107.102.5:3306/t2g5", "t2g5", "ttYMT5++");
-      connection = DriverManager.getConnection("jdbc:mariadb://localhost/compus", "root", "P@chelb3L_2256");
+      connection = DriverManager.getConnection("jdbc:mariadb://143.107.102.5:3306/t2g5", "t2g5", "ttYMT5++");
     } catch (SQLException | ClassNotFoundException e) {
       e.printStackTrace();
     }
